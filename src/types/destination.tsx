642519c7175c5e301bee5390a255fd68d8f665ee
Michael <michael.scully1997@gmail.com>
2022-07-26 01:18:38 -0400

    Add Destination Model

---
+++ src/types/destination.tsx
@@ -0,0 +1,9 @@
+export default interface Destination {
+  name: string;
+  placeId: string;
+  country: string;
+  countryCode: string;
+  latitude: number;
+  longitude: number;
+  type: string;
+}
