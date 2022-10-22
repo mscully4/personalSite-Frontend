1e8b86586cd9668aeb96c2e4edee2423421cff20
Michael <michael.scully1997@gmail.com>
2022-10-22 15:54:23 -0400

    Update Nav Menu to Be More Responsive
    Have the Nav Menu resize as the width of the screen changes to prevent
    stuff getting smushed on small screens

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Travel from "./views/travel";
 import Resume from "./views/resume";
 import Navigation from "./components/navigation";
 import { lightTheme } from "./utils/colors";
+import { BreakpointKeys, breakpoints, Orientation } from "./utils/display";
+import { useMediaQuery } from "@mui/material";
 
 const NAV_HEIGHT_PERC = 0.075;
 const MIN_NAV_HEIGHT_PX = 64;
@@ -17,9 +19,37 @@ function App() {
     if (window.innerHeight !== height) setHeight(window.innerHeight);
   });
   const navHeight = Math.max(height * NAV_HEIGHT_PERC, MIN_NAV_HEIGHT_PX);
+
+  const mediaQueries: Record<
+    Orientation,
+    Partial<Record<BreakpointKeys, boolean>>
+  > = {
+    [Orientation.WIDTH]: {
+      [BreakpointKeys.sm]: useMediaQuery(
+        `(min-width:${breakpoints[Orientation.WIDTH][BreakpointKeys.sm]}px)`
+      ),
+      [BreakpointKeys.md]: useMediaQuery(
+        `(min-width:${breakpoints[Orientation.WIDTH][BreakpointKeys.md]}px)`
+      ),
+      [BreakpointKeys.lg]: useMediaQuery(
+        `(min-width:${breakpoints[Orientation.WIDTH][BreakpointKeys.lg]}px)`
+      ),
+    },
+    [Orientation.HEIGHT]: {
+      [BreakpointKeys.sm]: useMediaQuery(
+        `(min-height:${breakpoints[Orientation.HEIGHT][BreakpointKeys.sm]}px)`
+      ),
+      [BreakpointKeys.md]: useMediaQuery(
+        `(min-height:${breakpoints[Orientation.HEIGHT][BreakpointKeys.md]}px)`
+      ),
+      [BreakpointKeys.lg]: useMediaQuery(
+        `(min-height:${breakpoints[Orientation.HEIGHT][BreakpointKeys.lg]}px)`
+      ),
+    },
+  };
   return (
     <ThemeProvider theme={lightTheme}>
-      <Navigation height={navHeight} />
+      <Navigation height={navHeight} mediaQueries={mediaQueries} />
       <div style={{ height: height - navHeight }}>
         <BrowserRouter>
           <Switch>
