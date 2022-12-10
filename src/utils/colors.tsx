27e8621720eb4df66671c65218f25aa411c2b3ed
Michael <michael.scully1997@gmail.com>
2022-12-10 17:33:05 -0500

    Add/Change Some Pin Colors

--- src/utils/colors.tsx
+++ src/utils/colors.tsx
@@ -16,14 +16,33 @@ export const foo = {
   blue: "#016aff",
 };
 
+// I tried to sort these in ROYGBIV order
 export const pinColors = [
-  "#0084FF",
-  "#44BEC7",
-  "#FFC300",
-  "#FA3C4C",
-  "#D696BB",
-  "#3FB1CE",
-  "#17A589", // Turquoise
+  // Reds
+  "#8B0020", // Dark red
+  "#F42C04", // Scarlet
+  "#FA3C4C", // Salmon
+  // Oranges
+  "#F28500", // Tangerine
+  "#F6B092", // Peach-ish
+  // Yellow
+  "#FFB30F", // Honey
+  "#E9D758", // Arylide
+  "#FFD700", // Gold
+  // Green
+  "#849324", // Olive
+  "#23CE6B", // Emerald
+  "#80FF72", // Neon
+  // Blue
+  "#0084FF", // A Darkish blue
+  "4D9DE0", // Carolona
+  "#3FB1CE", // Turqouise
+  "#8EEDF7", // Electric
+  // Purple
+  "#B892FF", // Valendar
+  "#7B4B94", // Royal Purple
+  "#7D82B8", // Middle Blue Purple
+  "#6153CC", // Purple
 ];
 
 export const randomPinColor = () => {
